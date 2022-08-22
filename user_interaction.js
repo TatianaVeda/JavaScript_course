81ea0d606eeac0404b0afd1dace90aea6ca8d9f9
Tanja <33755571+TatianaVeda@users.noreply.github.com>
2022-08-22 19:39:31 +0300

    Add files via upload

---
+++ user_interaction.js
@@ -0,0 +1,23 @@
+"use sctrict";
+
+// alert('Terve!');
+
+//const result = confirm("Oletko siellä?");
+//console.log(result);
+
+//const answer = +prompt ("Are you adult?", "18");
+//console.log(answer + 5);
+//console.log(typeof (answer));
+
+const answers = [];
+
+answers[0] = prompt('What is your name?', '');
+answers[1] = prompt('What is your lastname?', '');
+answers[2] = prompt('How old are you?', '');
+
+//document.write('Nothing left to say now');
+document.write(answers);
+
+console.log(typeof(answers));
+
+
